12ff9d16cbb24a89617c61bc016b57520a0bffc2
Juansa77 <dedalus1977@gmail.com>
2023-05-08 13:40:58 +0200

    Summarization test

--- promptTest.py
+++ promptTest.py
@@ -6,6 +6,9 @@ openai.api_key  = os.getenv("OPENAI_API_KEY")
 
 #FUNCIÓN PARA VISUALIZAR LAS PROMPTS Y LAS RESPUESTAS
 
+
+
+
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
@@ -16,11 +19,22 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     return response.choices[0].message["content"]
 
 text = f"""
-text value
+Got this panda plush toy for my daughter's birthday, \
+who loves it and takes it everywhere. It's soft and \ 
+super cute, and its face has a friendly look. It's \ 
+a bit small for what I paid though. I think there \ 
+might be other options that are bigger for the \ 
+same price. It arrived a day earlier than expected, \ 
+so I got to play with it myself before I gave it \ 
+to her.
 """
 prompt = f"""
-prompt
-```{text}```
+Your task is to generate a short summary of a product \
+review from an ecommerce site. 
+
+Summarize the review below, delimited by triple 
+backticks, in at most 30 words. 
+Review:```{text}```
 """
 
 #USAMOS TRIPLES COMILLAS EN LA VARIABLE TEXTO PARA QUE EL MODELO ENTIENDA QUE ES UNA SECCIÓN SEPARADA
